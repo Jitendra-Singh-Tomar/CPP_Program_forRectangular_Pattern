14192671d1d2e4b68ab87030e742831796d17d95
Jitendra-Singh-Tomar <119444147+Jitendra-Singh-Tomar@users.noreply.github.com>
2022-12-03 20:27:43 +0530

    Add files via upload
    // C++ program to print following rectangular pattern of numbers
    // provide the input: a number till which you want the pattern to be printed
    for input:5 
    output:
    12345
    23451
    34512
    45123
    51234

---
+++ patternNum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ // program to print rectangular pattern of numbers
+
+ int num;
+ cout << "enter one number:" <<endl;
+ cin >> num;
+ cout << "The pattern is:" <<endl;
+  for(int i=1; i<=num; i++){
+    for(int j=i; j<=num; j++){
+        cout <<j;
+    }
+    for(int m=1; m<=(i-1); m++){
+        cout <<m;
+    }
+    cout <<endl;
+  }
+
+}
